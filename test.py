e7745cbcc7dbdddb385eac690fd2b604e8d6ff5e
Abhishek Singh <44022825+Abhsin@users.noreply.github.com>
2023-12-07 15:16:20 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,44 +1,76 @@
-from logic import check_winner
-import pytest
-from cli import TicTacToeGame
+import unittest
+from logic import TicTacToeLogic
+from cli import TicTacToeCLI
 
-def test_game_initialization():
-    game = TicTacToeGame()
-    assert game.board == [[None, None, None], [None, None, None], [None, None, None]]
-    assert game.current_player == 'X'
-    assert game.winner is None
 
-def test_player_assignment():
-    game = TicTacToeGame()
-    assert game.current_player == 'X'
-    game.switch_player()
-    assert game.current_player == 'O'
-    game.switch_player()
-    assert game.current_player == 'X'
+class TestLogic(unittest.TestCase):
 
-def test_valid_moves():
-    game = TicTacToeGame()
-    assert game.make_empty_board() == [[None, None, None], [None, None, None], [None, None, None]]
-    row, col = game.get_player_input(player_input="0,0")
-    assert (row, col) in [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)]
+#   Check for draw conditions
+    def test_get_winner_DRAW(self):
+        board = [
+            ['X', 'X', 'O'],
+            ['O', 'X', 'X'],
+            ['X', 'O', 'O'],
+        ]
+        self.assertEqual(TicTacToeLogic.get_winner(self, board), 'Draw')
 
-def test_invalid_moves():
-    game = TicTacToeGame()
-    game.board[0][0] = 'X'
-    with pytest.raises(ValueError):
-        game.get_player_input(player_input="0,0")
+    def test_get_winner_horizontal(self):
+        board_1 = [
+            ['X', 'X', 'X'],
+            ['O', 'O', 'X'],
+            ['O', 'X', 'O'],
+        ]
+        self.assertEqual(TicTacToeLogic.get_winner(self, board_1), 'X')
 
-def test_game_winner():
-    game = TicTacToeGame()
-    game.board = [['X', 'O', 'O'], [None, 'X', 'O'], ['X', None, 'X']]
-    assert check_winner(game.board) == 'X'
+    def test_get_winner(self):
+        board_1 = [
+            ['O', 'O', 'X'],
+            ['O', 'X', 'O'],
+            ['X', 'O', 'O'],
+        ]
+        self.assertEqual(TicTacToeLogic.get_winner(self, board_1), 'X')
 
-def test_draw_game():
-    game = TicTacToeGame()
-    game.board = [['X', 'O', 'X'], ['X', 'O', 'O'], ['O', 'X', 'X']]
-    assert check_winner(game.board) is 'draw'
+    def test_get_winer_lower(self):
+        board_1 = [
+            ['O', 'O', 'X'],
+            ['X', 'X', 'X'],
+            ['O', 'X', 'O'],
+        ]
+        self.assertEqual(TicTacToeLogic.get_winner(self, board_1), 'X')
 
-# Add more test cases as needed
+
+# After one player plays, the other player has a turn
+    def test_other_player(self):
+        player = 'O'
+        self.assertEqual(TicTacToeLogic.other_player(self,player=player),'X')
+
+        
+    def test_update_board(self):
+        board = [
+            ['X', None, 'O'],
+            [None, 'X', None],
+            [None, 'O', None],
+        ]
+        test_case_board = ([['X', 'X', 'O'],[None, 'X', None],[None, 'O', None]],False)
+        self.assertEqual(TicTacToeLogic.update_board(self, board,i=1,j=2,char='X'),test_case_board)
+
+# Players can play only in viable spots
+    def test_illegal_update_board(self):
+        board = [
+            ['X', None, 'O'],
+            [None, 'X', None],
+            [None, 'O', None],
+        ]
+        #Assert for illegal Move
+        test_case_board = ([['X', None, 'O'],[None, 'X', None],[None, 'O', None]],True)
+        self.assertEqual(TicTacToeLogic.update_board(self, board,i=1,j=1,char='X'),test_case_board)
+
+# The game is initialized with an empty board
+    def test_intialized_empty(self):
+        board = [[None, None, None],
+            [None, None, None],
+            [None, None, None],]
+        self.assertEqual(TicTacToeLogic.make_empty_board(self),board)
 
 if __name__ == '__main__':
-    pytest.main(['test.py'])
+    unittest.main()
